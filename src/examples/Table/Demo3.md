4cf5fbddf5f8d8dd64c7705843d5a6c74689ed07
zenghuiyan <huiyanzeng0304@163.com>
2018-04-13 09:30:11 +0800

    Radio-button大小修改，table添加子表格

---
+++ src/examples/Table/Demo3.md
@@ -0,0 +1,90 @@
+#  嵌套子表格
+## 展示每行数据更详细的信息
+
+
+````jsx
+import Table from '@/components/Table'
+
+export class <%=component%> extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            expandKey:[]
+        }
+    }
+    onExpand(expandKeys,record){
+        this.setState({
+            expandKey:expandKeys
+        })
+    }
+    render() {
+      const expandedRowRender = (record) => {
+        const columns = [
+          { title: 'Date', dataIndex: 'date', key: 'date' },
+          { title: 'Name', dataIndex: 'name', key: 'name' },
+          { title: 'Status', key: 'state', render: () => <span>Finished</span> },
+          { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+          {
+            title: 'Action',
+            dataIndex: 'operation',
+            key: 'operation',
+            render: () => (
+              <span className="table-operation">
+                <a href="#">Pause</a>
+              </span>
+            ),
+          },
+        ];
+
+        const data4 = [];
+        for (let i = 0; i < 3; ++i) {
+          data4.push({
+            key: i+'0',
+            date: '2014-12-24 23:12:00',
+            name: 'This is production name',
+            upgradeNum: 'Upgraded: 56',
+          });
+        }
+        if(record.key === 0){
+         return (
+                  <Table
+                    columns={columns}
+                    rowKey='key'
+                    dataSource={data4}
+                    pagination={false}
+                  />
+                );
+        }
+
+      };
+       const columns = [
+           { title: 'Name', dataIndex: 'name', key: 'name' },
+           { title: 'Platform', dataIndex: 'platform', key: 'platform' },
+           { title: 'Version', dataIndex: 'version', key: 'version' },
+           { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+           { title: 'Creator', dataIndex: 'creator', key: 'creator' },
+           { title: 'Action', key: 'operation', render: () => <a href="#">Publish</a> },
+         ];
+
+         const data3 = [];
+         for (let i = 0; i < 3; ++i) {
+           data3.push({
+             key: i,
+             name: 'Screem',
+             platform: 'iOS',
+             version: '10.3.4.5654',
+             upgradeNum: 500,
+             creator: 'Jack',
+             createdAt: '2014-12-24 23:12:00',
+           });
+         }
+
+        return (
+            <div className='table-example' style={{background:'#fff',padding:20}}>
+                <Table columns={columns}  expandedRowRender={expandedRowRender} expandKey={this.state.expandKey}
+                onExpand={(expandKeys,record)=>this.onExpand(expandKeys,record)} rowKey={'key'}  dataSource={data3}/>
+            </div>
+        )
+    }
+}
+````
--- src/examples/Table/index.js
+++ src/examples/Table/index.js
@@ -16,7 +16,13 @@ let data1 = [{
               key: '7',
               name: '胡彦祖7',
               age: 88,
-              address: '西湖区湖底公园2号'
+              address: '西湖区湖底公园2号',
+               children:[{
+                            key: 'ss',
+                            name: 'fff',
+                            age: 88,
+                            address: '混蛋混蛋'
+                            }]
               }]
 }, {
     key: '2',
@@ -53,7 +59,14 @@ let data1 = [{
 export class Demo1md extends Component {
     constructor(props){
         super(props)
-
+        this.state = {
+            expandKey:[]
+        }
+    }
+    onExpand(expandKeys,record){
+        this.setState({
+            expandKey:expandKeys
+        })
     }
     render() {
         const columns = [{
@@ -77,7 +90,8 @@ export class Demo1md extends Component {
         }];
         return (
             <div className='table-example' style={{background:'#fff',padding:20}}>
-                <Table columns={columns} rowKey={'key'}  dataSource={data1}/>
+                <Table columns={columns} expandKey={this.state.expandKey}
+                onExpand={(expandKeys,record)=>this.onExpand(expandKeys,record)} rowKey={'key'}  dataSource={data1}/>
             </div>
         )
     }
@@ -170,6 +184,90 @@ export class Demo2md extends Component {
 }
 
 
+export class Demo3md extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            expandKey:[]
+        }
+    }
+    onExpand(expandKeys,record){
+        this.setState({
+            expandKey:expandKeys
+        })
+    }
+    render() {
+      const expandedRowRender = (record) => {
+        const columns = [
+          { title: 'Date', dataIndex: 'date', key: 'date' },
+          { title: 'Name', dataIndex: 'name', key: 'name' },
+          { title: 'Status', key: 'state', render: () => <span>Finished</span> },
+          { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+          {
+            title: 'Action',
+            dataIndex: 'operation',
+            key: 'operation',
+            render: () => (
+              <span className="table-operation">
+                <a href="#">Pause</a>
+              </span>
+            ),
+          },
+        ];
+
+        const data4 = [];
+        for (let i = 0; i < 3; ++i) {
+          data4.push({
+            key: i+'0',
+            date: '2014-12-24 23:12:00',
+            name: 'This is production name',
+            upgradeNum: 'Upgraded: 56',
+          });
+        }
+        if(record.key === 0){
+         return (
+                  <Table
+                    columns={columns}
+                    rowKey='key'
+                    dataSource={data4}
+                    pagination={false}
+                  />
+                );
+        }
+
+      };
+       const columns = [
+           { title: 'Name', dataIndex: 'name', key: 'name' },
+           { title: 'Platform', dataIndex: 'platform', key: 'platform' },
+           { title: 'Version', dataIndex: 'version', key: 'version' },
+           { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+           { title: 'Creator', dataIndex: 'creator', key: 'creator' },
+           { title: 'Action', key: 'operation', render: () => <a href="#">Publish</a> },
+         ];
+
+         const data3 = [];
+         for (let i = 0; i < 3; ++i) {
+           data3.push({
+             key: i,
+             name: 'Screem',
+             platform: 'iOS',
+             version: '10.3.4.5654',
+             upgradeNum: 500,
+             creator: 'Jack',
+             createdAt: '2014-12-24 23:12:00',
+           });
+         }
+
+        return (
+            <div className='table-example' style={{background:'#fff',padding:20}}>
+                <Table columns={columns}  expandedRowRender={expandedRowRender} expandKey={this.state.expandKey}
+                onExpand={(expandKeys,record)=>this.onExpand(expandKeys,record)} rowKey={'key'}  dataSource={data3}/>
+            </div>
+        )
+    }
+}
+
+
 export default class App extends Component {
     render() {
         return (
@@ -192,7 +290,13 @@ let data1 = [{
               key: '7',
               name: '胡彦祖7',
               age: 88,
-              address: '西湖区湖底公园2号'
+              address: '西湖区湖底公园2号',
+               children:[{
+                            key: 'ss',
+                            name: 'fff',
+                            age: 88,
+                            address: '混蛋混蛋'
+                            }]
               }]
 }, {
     key: '2',
@@ -229,7 +333,14 @@ let data1 = [{
 export class Demo1md extends Component {
     constructor(props){
         super(props)
-
+        this.state = {
+            expandKey:[]
+        }
+    }
+    onExpand(expandKeys,record){
+        this.setState({
+            expandKey:expandKeys
+        })
     }
     render() {
         const columns = [{
@@ -253,7 +364,8 @@ export class Demo1md extends Component {
         }];
         return (
             <div className='table-example' style={{background:'#fff',padding:20}}>
-                <Table columns={columns} rowKey={'key'}  dataSource={data1}/>
+                <Table columns={columns} expandKey={this.state.expandKey}
+                onExpand={(expandKeys,record)=>this.onExpand(expandKeys,record)} rowKey={'key'}  dataSource={data1}/>
             </div>
         )
     }
@@ -354,6 +466,99 @@ export class Demo2md extends Component {
     }
 }`}>
                         <Demo2md />
+                    </Templete>
+                
+                     <Typography variant="display1" gutterBottom>
+                                      嵌套子表格
+                                </Typography>
+                                <p style={{margin:'24px 0 12px 0'}}>
+                                     展示每行数据更详细的信息
+                                </p>
+                    <Templete code={`import Table from '@/components/Table'
+
+export class Demo3md extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            expandKey:[]
+        }
+    }
+    onExpand(expandKeys,record){
+        this.setState({
+            expandKey:expandKeys
+        })
+    }
+    render() {
+      const expandedRowRender = (record) => {
+        const columns = [
+          { title: 'Date', dataIndex: 'date', key: 'date' },
+          { title: 'Name', dataIndex: 'name', key: 'name' },
+          { title: 'Status', key: 'state', render: () => <span>Finished</span> },
+          { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+          {
+            title: 'Action',
+            dataIndex: 'operation',
+            key: 'operation',
+            render: () => (
+              <span className="table-operation">
+                <a href="#">Pause</a>
+              </span>
+            ),
+          },
+        ];
+
+        const data4 = [];
+        for (let i = 0; i < 3; ++i) {
+          data4.push({
+            key: i+'0',
+            date: '2014-12-24 23:12:00',
+            name: 'This is production name',
+            upgradeNum: 'Upgraded: 56',
+          });
+        }
+        if(record.key === 0){
+         return (
+                  <Table
+                    columns={columns}
+                    rowKey='key'
+                    dataSource={data4}
+                    pagination={false}
+                  />
+                );
+        }
+
+      };
+       const columns = [
+           { title: 'Name', dataIndex: 'name', key: 'name' },
+           { title: 'Platform', dataIndex: 'platform', key: 'platform' },
+           { title: 'Version', dataIndex: 'version', key: 'version' },
+           { title: 'Upgraded', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+           { title: 'Creator', dataIndex: 'creator', key: 'creator' },
+           { title: 'Action', key: 'operation', render: () => <a href="#">Publish</a> },
+         ];
+
+         const data3 = [];
+         for (let i = 0; i < 3; ++i) {
+           data3.push({
+             key: i,
+             name: 'Screem',
+             platform: 'iOS',
+             version: '10.3.4.5654',
+             upgradeNum: 500,
+             creator: 'Jack',
+             createdAt: '2014-12-24 23:12:00',
+           });
+         }
+
+        return (
+            <div className='table-example' style={{background:'#fff',padding:20}}>
+                <Table columns={columns}  expandedRowRender={expandedRowRender} expandKey={this.state.expandKey}
+                onExpand={(expandKeys,record)=>this.onExpand(expandKeys,record)} rowKey={'key'}  dataSource={data3}/>
+            </div>
+        )
+    }
+}`}>
+                        <Demo3md />
                     </Templete>
                 
                 <Api />
